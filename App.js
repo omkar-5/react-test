177db29f79aab01b1099195b1fa771bbf6950cb1
omkar-5 <omkarpansare05@gmail.com>
2024-10-31 19:24:58 +0530

    Create react app

--- App.js
+++ App.js
@@ -1,17 +1,12 @@
-/*
-<div id="parent">
-    <div id = "child">
-        <h1 id="head">I am h1 tag</h1>
-    </div>
-</div>
-*/
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 
 const heading = React.createElement("h1",{id:"heading", abc:"xyz"},"Hello from react");
-//console.log(heading);
+
             const root = ReactDOM.createRoot(document.getElementById("root"));
             const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{id:"head"},"I am h1 tag")))
-            //console.log(root);
-           // root.render(parent);
+
 
 
             /*
